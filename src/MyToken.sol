649e7c86aaf88528bb138dd16dc765d5ae78f700
cp007 <captpython007@gmail.com>
2024-02-14 07:26:24 +0800

    changes

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -4,7 +4,9 @@
  import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
  contract MyToken is ERC20 {
-
+    constructor() ERC20("Core Token", "CMN") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
 
  }
 
